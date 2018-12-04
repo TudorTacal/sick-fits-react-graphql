e0c9613f19f01934ddf154ca78815451ff2d9ea0
Tudor Tacal <t.tacal@elsevier.com>
2018-12-04 00:00:29 +0000

    Get up and running with NextJS. Create app layout.

---
+++ frontend/components/Header.js
@@ -0,0 +1,17 @@
+import Nav from './Nav';
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">Sick Fits
+      <Nav />
+      </a>
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+)
+
+export default Header;

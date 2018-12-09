19989c66c0c6e87b69a9be8b3bf90d5823b6bbf5
Tudor Tacal <tudortacal@gmail.com>
2018-12-09 23:53:01 +0000

    Run GraphQL Yoga server
    - create Prisma DB instance
    - define schema.graphql
    - instantiate yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO Use express middleware to ahndle cookies (JWT)
+// TODO Use express middleware to populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  },
+}, deets => {
+  console.log(`Server is not running on port http://localhosT:${deets.port}`)
+});

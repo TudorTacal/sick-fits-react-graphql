f98b649bb19c6ab7047d77def12541e47a9eb4aa
Tudor Tacal <tudortacal@gmail.com>
2018-12-10 20:46:29 +0000

    Write the first Query and Mutation
    - use global to play with data
    - setup Eslint

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,6 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga');
 const Mutation = require('./resolvers/Mutation');
-const Query = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
 const db = require('./db');
 // Create the GraphQL server
 
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    // craete a dog
+    const newDog = { 
+      name: args.name,
+    };
+    global.dogs.push(newDog);
+    return newDog;
+    console.log(args);
+  }
+};
 
 module.exports = Mutations;

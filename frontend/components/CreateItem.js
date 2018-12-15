c1a6424d0a7dd24cb4a969a7799fbbca9af8ae09
Tacal <tudortacal@gmail.com>
2018-12-15 21:45:28 +0000

    Upload images using Cloudinary

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -38,6 +38,25 @@ class CreateItem extends Component {
     const val = type === Number ? parseFloat(value) : value;
     this.setState({ [name]: val });
   }
+
+  uploadFile = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/cloudinary9999/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
+  }
+
   render() {
     return (
       <Mutation
@@ -50,9 +69,8 @@ class CreateItem extends Component {
             const res = await createItem();
             Router.push({
               pathname: '/item',
-              query: { id: res.data.createItem.id}
+              query: { id: res.data.createItem.id }
             })
-            console.log(res);
           }}
           >
             <Error error={error} />
@@ -60,9 +78,28 @@ class CreateItem extends Component {
               disabled={loading}
               area-busy={loading.toString()}
             >
+              <label htmlFor="file">
+                Image
+             <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img src={this.state.image} alt="Upload preview"/>}
+              </label>
               <label htmlFor="title">
                 Title
-             <input type="text" id="title" name="title" placeholder="Title" required onChange={this.handleChange} value={this.state.title} />
+             <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  onChange={this.handleChange}
+                  value={this.state.title} />
               </label>
               <label htmlFor="price">
                 Price

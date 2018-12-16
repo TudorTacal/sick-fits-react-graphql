9dee99edb9cd8901aad23f36eb3000c5308861a5
Tacal <tudortacal@gmail.com>
2018-12-16 23:17:28 +0000

    Update items with Queries and Mutations

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import Router from 'next/router';
-import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
-import Form from './styles/Form';
-import formatMoney from '../lib/formatMoney';
-import Error from './ErrorMessage';
+import React, { Component } from "react";
+import Router from "next/router";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
+
 export const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
-    $title: String!
-    $description: String!
-    $price: Int!
-    $image: String
-    $largeImage: String
+    $title: String!,
+    $description: String!,
+    $price: Int!,
+    $image: String,
+    $largeImage: String,
   ) {
     createItem(
       title: $title
@@ -27,60 +28,58 @@ export const CREATE_ITEM_MUTATION = gql`
 
 class CreateItem extends Component {
   state = {
-    title: 'Cool shoes',
-    description: 'I love shoes',
-    image: '',
-    largeImage: '',
-    price: 10,
+    title: "Cool shoes",
+    description: "I love shoes",
+    image: "",
+    largeImage: "",
+    price: 10
   };
-  handleChange = (e) => {
+  handleChange = e => {
     const { name, type, value } = e.target;
     const val = type === Number ? parseFloat(value) : value;
     this.setState({ [name]: val });
-  }
+  };
 
-  uploadFile = async (e) => {
+  uploadFile = async e => {
     const files = e.target.files;
     const data = new FormData();
-    data.append('file', files[0]);
-    data.append('upload_preset', 'sickfits');
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
 
-    const res = await fetch('https://api.cloudinary.com/v1_1/cloudinary9999/image/upload', {
-      method: 'POST',
-      body: data,
-    });
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/cloudinary9999/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    );
 
     const file = await res.json();
     this.setState({
       image: file.secure_url,
-      largeImage: file.eager[0].secure_url,
-    })
-  }
+      largeImage: file.eager[0].secure_url
+    });
+  };
 
   render() {
     return (
-      <Mutation
-        mutation={CREATE_ITEM_MUTATION}
-        variables={this.state}
-      >
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
-          <Form onSubmit={async (e) => {
-            e.preventDefault();
-            const res = await createItem();
-            Router.push({
-              pathname: '/item',
-              query: { id: res.data.createItem.id }
-            })
-          }}
+          <Form
+            onSubmit={async e => {
+              e.preventDefault();
+              const res = await createItem();
+              Router.push({
+                pathname: "/item",
+                query: { id: res.data.createItem.id }
+              });
+            }}
           >
             <Error error={error} />
-            <fieldset
-              disabled={loading}
-              area-busy={loading.toString()}
-            >
+            <fieldset disabled={loading} area-busy={loading.toString()}>
               <label htmlFor="file">
                 Image
-             <input
+                <input
                   type="file"
                   id="file"
                   name="file"
@@ -88,30 +87,47 @@ class CreateItem extends Component {
                   required
                   onChange={this.uploadFile}
                 />
-                {this.state.image && <img src={this.state.image} alt="Upload preview"/>}
+                {this.state.image && (
+                  <img src={this.state.image} alt="Upload preview" />
+                )}
               </label>
               <label htmlFor="title">
                 Title
-             <input
+                <input
                   type="text"
                   id="title"
                   name="title"
                   placeholder="Title"
                   required
                   onChange={this.handleChange}
-                  value={this.state.title} />
+                  value={this.state.title}
+                />
               </label>
               <label htmlFor="price">
                 Price
-             <input type="number" id="price" name="price" placeholder="Price" required onChange={this.handleChange} value={this.state.price} />
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  onChange={this.handleChange}
+                  value={this.state.price}
+                />
               </label>
               <label htmlFor="description">
                 Description
-             <input type="text-area" id="description" name="description" placeholder="Enter a description" required onChange={this.handleChange} value={this.state.description} />
+                <input
+                  type="text-area"
+                  id="description"
+                  name="description"
+                  placeholder="Enter a description"
+                  required
+                  onChange={this.handleChange}
+                  value={this.state.description}
+                />
               </label>
-              <button type="submit">
-                Submit
-           </button>
+              <button type="submit">Submit</button>
             </fieldset>
           </Form>
         )}
---
+++ frontend/components/UpdateItem.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import Router from "next/router";
+import { Mutation, Query } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+export const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    $id: ID!
+    $title: String
+    $description: String
+    $price: Int
+  ) {
+    updateItem(
+      id: $id
+      title: $title
+      description: $description
+      price: $price
+    ) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+
+class UpdateItem extends Component {
+  state = {};
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === Number ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+  updateItem = async (e, updateItemMutation) => {
+    e.preventDefault();
+    console.log("Updating Item", this.props.id);
+    console.log(this.state);
+
+    const res = await updateItemMutation({
+      variables: {
+        id: this.props.id,
+        ...this.state
+      }
+    });
+  };
+
+  render() {
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({ data, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No item found for ID not real</p>;
+          return (
+            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+              {(updateItem, { loading, error }) => (
+                <Form onSubmit={e => this.updateItem(e, updateItem)}>
+                  <Error error={error} />
+                  <fieldset disabled={loading} area-busy={loading.toString()}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        onChange={this.handleChange}
+                        defaultValue={data.item.title}
+                      />
+                    </label>
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        required
+                        onChange={this.handleChange}
+                        defaultValue={data.item.price}
+                      />
+                    </label>
+                    <label htmlFor="description">
+                      Description
+                      <input
+                        type="text-area"
+                        id="description"
+                        name="description"
+                        placeholder="Enter a description"
+                        required
+                        onChange={this.handleChange}
+                        defaultValue={data.item.description}
+                      />
+                    </label>
+                    <button type="submit">Sav{loading ? 'ing' : 'e'}</button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default UpdateItem;

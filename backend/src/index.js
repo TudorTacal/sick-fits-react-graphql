796e2b4965cdf4eb5221af94dbcfa2db9e104d2a
Tacal <tudortacal@gmail.com>
2018-12-17 00:05:17 +0000

    Update schema Mutation type with deleteItem field and Mutation resolver with deleteItem method

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,20 @@
-require('dotenv').config({ path: 'variables.env'});
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
 // TODO Use express middleware to ahndle cookies (JWT)
 // TODO Use express middleware to populate current user
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
   },
-}, deets => {
-  console.log(`Server is not running on port http://localhosT:${deets.port}`)
-});
+  deets => {
+    console.log(`Server is now running on port http://localhosT:${deets.port}`);
+  }
+);
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,11 +1,14 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
       },
-    }, info);
+      info
+    );
     console.log(item);
 
     return item;
@@ -16,12 +19,25 @@ const Mutations = {
     // remove the id from the updates
     delete updates.id;
     // run the update method
-    return ctx.db.mutation.updateItem({
-      data: updates,
-      where: {
-        id: args.id,
-      }
-    }, info);
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    console.log(args.id);
+
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`); // info - takes query from the client like (id, title, etc) and return that data
+    // 2. check if they own that item or have the permisssions
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 

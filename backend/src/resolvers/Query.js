0c72225487a278bfccc1c863d7ac36d403f60f18
Tacal <tudortacal@gmail.com>
2018-12-21 22:34:00 +0000

    Add itemsConnection to Query schema; Update Query resolver to forward to Prisma

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,12 +1,13 @@
-const { forwardTo } = require('prisma-binding'); // the ability to query the database
+const { forwardTo } = require("prisma-binding"); // the ability to query the database
 
 const Query = {
-  items: forwardTo('db'),
-  item: forwardTo('db'),
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db")
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
-  // } 
+  // }
 };
 
 module.exports = Query;

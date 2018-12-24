b29a6735909208cdc88179b005c4db9da3e6b3c0
Tacal <tudortacal@gmail.com>
2018-12-24 00:19:20 +0000

    Currently Logged In User with Middleware and Render Props - add middleware to attack the userId on every req - crete me resolver - update schema with me field

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,31 @@
-const cookieParser = require("cookie-parser");
-require("dotenv").config({ path: "variables.env" });
-const createServer = require("./createServer");
-const db = require("./db");
+const cookieParser = require('cookie-parser');
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+const jwt = require('jsonwebtoken');
 
 const server = createServer();
 
-// TODO Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
-// TODO Use express middleware to populate current user
+
+// decode the JWT so we can get the user id on each request
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
+      origin: process.env.FRONTEND_URL,
+    },
   },
   deets => {
     console.log(`Server is now running on port http://localhosT:${deets.port}`);

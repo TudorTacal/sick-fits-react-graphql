e387dbee4e28852d4e7867f55071c68a165f241f
Tacal <tudortacal@gmail.com>
2018-12-28 23:19:03 +0000

    Server Side Add To Cart

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,6 +6,7 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
 import DeleteItem from './DeleteItem';
+import AddToCart from './AddToCart';
 
 class Item extends Component {
   static propTypes = {
@@ -14,6 +15,7 @@ class Item extends Component {
       price: PropTypes.number.isRequired,
     }),
   };
+
   render() {
     const { item } = this.props;
     return (
@@ -40,7 +42,7 @@ class Item extends Component {
           >
             <a>Edit ✏️</a>
           </Link>
-          <button>Add to Cart</button>
+          <AddToCart id={item.id} />
           <DeleteItem id={item.id}>Delete this item</DeleteItem>
         </div>
       </ItemStyles>

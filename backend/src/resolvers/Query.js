dba5b32ff511d3f421503eb25f9dd1f2881c5d81
Tacal <tudortacal@gmail.com>
2019-01-02 00:10:45 +0000

    Orders page

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -51,6 +51,20 @@ const Query = {
     // return the order
     return order;
   },
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('You must be signed in!');
+    }
+    return ctx.db.query.orders(
+      {
+        where: {
+          user: { id: userId },
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
